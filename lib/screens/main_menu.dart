9d631f6e603e763454b17a97a6a58fb165bed74e
Estid Felipe Lozano Reyes <estid_l@hotmail.com>
2020-10-22 16:50:56 -0500

    pull error in main_menu fixed

--- lib/screens/main_menu.dart
+++ lib/screens/main_menu.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'package:pietrario_sample_app/screens/market.dart';
 import 'package:pietrario_sample_app/util/Assets.dart';
-=======
 import 'package:pietrario_sample_app/screens/settings.dart';
->>>>>>> juan
 
 class MainMenu extends StatefulWidget {
   @override
@@ -16,55 +13,40 @@ class _MainMenuState extends State<MainMenu> {
     final double w = MediaQuery.of(context).size.width;
     final double h = MediaQuery.of(context).size.height;
     return Scaffold(
-<<<<<<< HEAD
       body: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              InkWell(
-                /*onTap:() => Navigator.push(context,
-                MaterialPageRoute(builder: (context) => PietrarioMenu())),*/
-                child: Image.asset(
-                  Assets.img('logo'),
-                  width: h / 8,
-                  height: h / 8,
-                ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            InkWell(
+              /*onTap:() => Navigator.push(context,
+              MaterialPageRoute(builder: (context) => PietrarioMenu())),*/
+              child: Image.asset(
+                Assets.img('logo'),
+                width: h / 8,
+                height: h / 8,
               ),
-              InkWell(
-                onTap:() => Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => Market())),
-                child: Image.asset(
-                  Assets.img('market'),
-                  width: h / 8,
-                  height: h / 8,
-                ),
+            ),
+            InkWell(
+              onTap:() => Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => Market())),
+              child: Image.asset(
+                Assets.img('market'),
+                width: h / 8,
+                height: h / 8,
               ),
-            ],
-          )
-=======
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          Image.asset(
-            'assets/img/leaf.png',
-            width: 100.0,
-            height: 100.0,
-          ),
-          RaisedButton(
-            child: Text('Settings'),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => Setting()),
-              );
-            },
-          )
-        ],
->>>>>>> juan
-      ),
+            ),
+            RaisedButton(
+              child: Text('Settings'),
+              onPressed: () {
+                Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => Setting()),
+                );
+              },
+            )
+          ],
+        ),
+      )
     );
   }
 }
