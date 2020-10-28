6c6de6226c368dc11156fd68c4d815732b3a3f0a
Wilson Andrés Camargo <49167124+wilsoncamgo@users.noreply.github.com>
2020-10-28 17:14:51 -0500

    Added SucculentMenu

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:pietrario_sample_app/screens/intro_page.dart';
 import 'package:pietrario_sample_app/screens/menu_dashboard.dart';
+import 'package:pietrario_sample_app/screens/succulent_menu.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: MenuDashboard(),
-  ));
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SucculentMenu(),
+    ),
+  );
 }
 
 class HelloWorld extends StatefulWidget {
